eba76e9bcbb425c2d1a8b028adadb1f7c188ef90
Luke Denton <denno020@gmail.com>
2023-05-04 22:34:26 +0930

    Update links on product card, remove unused refs

--- packages/base/components/ProductCard.jsx
+++ packages/base/components/ProductCard.jsx
@@ -3,29 +3,18 @@ import { Link } from "react-router-dom";
 import './ProductCard.css'
 
 const ProductCard = (props) => {
-  const { product, onSelect = () => {}, restorationRef, isSkeleton } = props;
-  const { id, name = 'Placeholder name', price = '10.00', image, pageNo } = product || {};
-
-  React.useEffect(() => {
-    // restorationRef is only provided to the ProductCard that needs to be scrolled to
-    if (!restorationRef) {
-      return;
-    }
-
-    // Restoring scroll here ensures the previously selected product will always be restored, no matter how long the API request
-    // to get products takes
-    restorationRef.current.scrollIntoView({ behavior: 'auto', block: 'center' });
-  })
+  const { product } = props;
+  const { id, name = 'Placeholder name', price = '10.00', image } = product || {};
 
   return (
-    <div className={`product-card ${isSkeleton ? 'product-card--skeleton' : ''}`} ref={restorationRef}>
+    <div className="product-card">
       <div className="product-card__image-container">
-        <Link to="/pdp" onClick={() => onSelect(id, pageNo)}>
+        <Link to={`/product/${id}`}>
           <img className="product-card__image" src={image} alt={name} />
         </Link>
       </div>
       <div className="product-card__name">
-        <Link to="/pdp" onClick={() => onSelect(id, pageNo)}>
+        <Link to={`/product/${id}`}>
           {name}
         </Link>
       </div>
