773d44eaadd9765d866c09b2886739531eacbb5e
Luke Denton <denno020@gmail.com>
2023-05-04 20:49:00 +0930

    Remove crap from get-items function

--- packages/base/util/get-items.js
+++ packages/base/util/get-items.js
@@ -4,14 +4,6 @@ const delay = 300;
 
 export const getItems = ({ page }) => {
   return fetch(`http://localhost:3000/products/${page}`).then(res => res.json()).then((res) => {
-    console.log({res})
     return res;
   });
-  return new Promise((res) => {
-    setTimeout(() => {
-      console.log(`API: getItems, page ${page}, loaded in ${delay}ms`);
-      const products = loadPage(page);
-      res({ items: products, totalPages: 10 });
-    }, delay);
-  })
 }
