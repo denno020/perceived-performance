ce59787858ead4a9c62746f184d810abc1bda8ab
Luke Denton <denno020@gmail.com>
2023-05-08 00:03:49 +0930

    Move PDP logic to a talon

--- packages/base/components/PDP/PDP.jsx
+++ packages/base/components/PDP/PDP.jsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useStore } from '../../store'
 import ProductCard from "../ProductCard";
 import LoadingIndicator from "../LoadingIndicator";
+import { usePDP } from './talons/usePDP'
 import classes from './PDP.module.css';
 
 const PDP = () => {
-  const [product, setProduct] = useState();
-  const [related, setRelated] = useState([]);
-  const [isAddingToCart, setIsAddingToCart] = useState(false);
-  const { setCart, toggleIsCartVisible } = useStore();
-  const location = useLocation();
-
-  useEffect(() => {
-    const productId = Number(location.pathname.replace('/product/', ''));
-    fetch(`http://localhost:3000/product/${productId}`).then(res => res.json()).then((res) => {
-      setProduct(res.product)
-    });
-
-    fetch(`http://localhost:3000/related`).then(res => res.json()).then((res) => {
-      setRelated(res.related)
-    });
-  }, [location.pathname])
-
-  const handleAddToCart = () => {
-    setIsAddingToCart(true);
-    fetch('http://localhost:3000/addToCart', {
-      method: 'post', headers: {
-        "Content-Type": "application/json",
-      }, body: JSON.stringify({ productId: product.id })
-    }).then(res => res.json()).then((res) => {
-      const cart = res.cart;
-      setCart(cart);
-      setIsAddingToCart(false);
-      toggleIsCartVisible();
-      setTimeout(() => {
-        toggleIsCartVisible();
-      }, 1000)
-    })
-  }
+  const {product, handleAddToCart, isAddingToCart, related} = usePDP();
 
   if (!product) {
     return <div className={classes.loadingContainer}>
---
+++ packages/base/components/PDP/talons/usePDP.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useStore } from "../../../store/index.js";
+import { useLocation } from "react-router-dom";
+
+export const usePDP = () => {
+  const [product, setProduct] = useState();
+  const [related, setRelated] = useState([]);
+  const [isAddingToCart, setIsAddingToCart] = useState(false);
+  const { setCart, toggleIsCartVisible } = useStore();
+  const location = useLocation();
+
+  useEffect(() => {
+    const productId = Number(location.pathname.replace('/product/', ''));
+    fetch(`http://localhost:3000/product/${productId}`).then(res => res.json()).then((res) => {
+      setProduct(res.product)
+    });
+
+    fetch(`http://localhost:3000/related`).then(res => res.json()).then((res) => {
+      setRelated(res.related)
+    });
+  }, [location.pathname])
+
+  const handleAddToCart = () => {
+    setIsAddingToCart(true);
+    fetch('http://localhost:3000/addToCart', {
+      method: 'post', headers: {
+        "Content-Type": "application/json",
+      }, body: JSON.stringify({ productId: product.id })
+    }).then(res => res.json()).then((res) => {
+      const cart = res.cart;
+      setCart(cart);
+      setIsAddingToCart(false);
+      toggleIsCartVisible();
+      setTimeout(() => {
+        toggleIsCartVisible();
+      }, 1000)
+    })
+  }
+
+  return {
+    product, handleAddToCart, isAddingToCart, related
+  }
+}
+
