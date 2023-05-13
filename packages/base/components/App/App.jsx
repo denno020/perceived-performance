c17a72cebf4543e4d0db8a55996488ccf5c855b2
Luke Denton <denno020@gmail.com>
2023-05-13 15:57:09 +0930

    Implement scroll to top on nav

--- packages/base/components/App/App.jsx
+++ packages/base/components/App/App.jsx
@@ -3,14 +3,17 @@ import Cart from "../Cart";
 import Header from "../Header";
 import Main from "../Main";
 import Footer from "../Footer";
+import { useScrollToTopOnNav } from "../../hooks/useScrollToTopOnNav";
 
-const App = () => (
+const App = () => {
+  useScrollToTopOnNav();
+  return(
   <>
     <Cart />
     <Header />
     <Main />
     <Footer />
   </>
-);
+)};
 
 export default App;
