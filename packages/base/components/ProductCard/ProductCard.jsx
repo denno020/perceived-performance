acd2dfbf6d81cd82b0f1a59cf833b892388a82d0
Luke Denton <denno020@gmail.com>
2023-05-14 09:04:38 +0930

    Update ProductCard with fetch first then navigate pattern demo

--- packages/base/components/ProductCard/ProductCard.jsx
+++ packages/base/components/ProductCard/ProductCard.jsx
@@ -1,21 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getItem } from "../../util/get-item.js";
 import './ProductCard.css'
 
 const ProductCard = (props) => {
   const { id, name, price, previewImage } = props.product;
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    getItem(id).then((product) => {
+      navigate(`/product/${id}`, { state: { product } })
+    })
+  }
+
+  const LinkComponent = (props) => {
+    const fetchFirst = false; // Set this to true to simulate fetching the product first before routing
+
+    if (fetchFirst) {
+      return <Link onClick={handleClick} to={`/product/${id}`}>{props.children}</Link>
+    }
+
+    return <Link to={`/product/${id}`}>{props.children}</Link>
+  }
 
   return (
     <div className="product-card">
       <div className="product-card__image-container">
-        <Link to={`/product/${id}`}>
+        <LinkComponent>
           <img className="product-card__image" src={previewImage} alt={name} />
-        </Link>
+        </LinkComponent>
       </div>
       <div className="product-card__name">
-        <Link to={`/product/${id}`}>
+        <LinkComponent>
           {name}
-        </Link>
+        </LinkComponent>
       </div>
       <div className="product-card__price">${price}</div>
     </div>
