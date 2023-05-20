b81855a8eea1a4728120d5898effb9fe1c462369
Luke Denton <denno020@gmail.com>
2023-05-20 21:23:53 +0930

    Remove the "fetch first" pattern from later examples

--- packages/02-push-fold-footer/components/ProductCard/ProductCard.jsx
+++ packages/02-push-fold-footer/components/ProductCard/ProductCard.jsx
@@ -1,34 +1,13 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { getItem } from "../../util/get-item.js";
-import './ProductCard.css'
+import { Link } from "react-router-dom";
+import "./ProductCard.css";
 
 const ProductCard = (props) => {
   const { id, name, price, previewImage } = props.product;
-  const navigate = useNavigate();
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    getItem(id).then((product) => {
-      navigate(`product/${id}`, { state: { product } })
-    })
-
-    // Really slow connections will be taken to the PDP before the network request is resolved
-    // @TODO Uncomment to demonstrate slow network connection
-    // setTimeout(() => {
-    //   navigate(`product/${id}`, { state: { product: false } })
-    // }, 500)
-  }
-
-  const LinkComponent = (props) => {
-    const fetchFirst = false; // Set this to true to simulate fetching the product first before routing
-
-    if (fetchFirst) {
-      return <Link onClick={handleClick} to={`product/${id}`}>{props.children}</Link>
-    }
-
-    return <Link to={`product/${id}`}>{props.children}</Link>
-  }
+  const LinkComponent = (props) => (
+    <Link to={`product/${id}`}>{props.children}</Link>
+  );
 
   return (
     <div className="product-card">
@@ -38,9 +17,7 @@ const ProductCard = (props) => {
         </LinkComponent>
       </div>
       <div className="product-card__name">
-        <LinkComponent>
-          {name}
-        </LinkComponent>
+        <LinkComponent>{name}</LinkComponent>
       </div>
       <div className="product-card__price">${price}</div>
     </div>
