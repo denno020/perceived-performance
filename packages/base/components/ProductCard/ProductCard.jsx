8ca2e6697d55740d359e8ccb26cca664aab8a971
Luke Denton <denno020@gmail.com>
2023-06-04 13:38:09 +0930

    Make the entire product card a link
    This means the whole thing can be clicked, not just the image/name

--- packages/base/components/ProductCard/ProductCard.jsx
+++ packages/base/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,10 @@
-import { useMemo } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useAppContext } from "../../contexts/AppContext.jsx";
-import { getItem } from "../../util/get-item";
-import { useStore } from "../../../base/store";
+import { useMemo } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../contexts/AppContext.jsx';
+import { getItem } from '../../util/get-item';
+import { useStore } from '../../../base/store';
 import { useProductCard } from './talons/useProductCard';
-import "./ProductCard.css";
+import './ProductCard.css';
 
 const ProductCard = (props) => {
   const { sectionId, fetchFirst, useCache } = useAppContext();
@@ -27,45 +27,49 @@ const ProductCard = (props) => {
     getItem(id, { useCache }).then((product) => {
       if (!location.pathname.includes('product')) {
         clearTimeout(timeoutId);
-        navigate(`${urlBase}/product/${id}`, { state: { product } })
+        navigate(`${urlBase}/product/${id}`, { state: { product } });
       }
     });
   };
 
-  const isCached = useMemo(
-    () => useCache && cachedProducts.includes(id),
-    [cachedProducts]
-  );
+  const isCached = useMemo(() => useCache && cachedProducts.includes(id), [cachedProducts]);
 
   const LinkComponent = (props) => {
+    const { children, ...rest } = props;
     if (fetchFirst) {
       return (
-        <Link onClick={(e) => {!isCached && handleClick(e)}} to={`${urlBase}/product/${id}`}>
-          {props.children}
+        <Link
+          onClick={(e) => {
+            !isCached && handleClick(e);
+          }}
+          to={`${urlBase}/product/${id}`}
+          {...rest}
+        >
+          {children}
         </Link>
       );
     }
 
-    return <Link to={`${urlBase}/product/${id}`}>{props.children}</Link>;
+    return (
+      <Link to={`${urlBase}/product/${id}`} {...rest}>
+        {children}
+      </Link>
+    );
   };
 
   return (
-    <div ref={ref} className={`product-card${isCached ? " product-cached" : ""}`}>
-      <div className="product-card__image-container">
-        <LinkComponent>
-          <img
-            className="product-card__image"
-            src={`${previewImage}?section=${sectionId}`}
-            alt={name}
-          />
-        </LinkComponent>
+    <LinkComponent className={`product-card${isCached ? ' product-cached' : ''}`}>
+      <div ref={ref} className="product-card__image-container">
+        <span>
+          <img className="product-card__image" src={`${previewImage}?section=${sectionId}`} alt={name} />
+        </span>
       </div>
       <div className="product-card__name">
-        <LinkComponent>{name}</LinkComponent>
+        <span>{name}</span>
       </div>
       <div className="product-card__price">${price}</div>
       {isCached && <div className="product-card-cached-icon">⚡</div>}
-    </div>
+    </LinkComponent>
   );
 };
 
