c5b5714b12e41c5fea1518d553ac8b88db2e5523
Luke Denton <luke.denton@ofx.com>
2025-02-12 11:32:02 +1030

    Revert "Set presentation step to 7 in store"
    This reverts commit 86770b859aa9cc4f0d664cb4ebedb0caff9cffb1.

--- packages/base/store/index.js
+++ packages/base/store/index.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 export const useStore = create((set) => ({
-  presentationStep: 7,
+  presentationStep: 0,
   cartVisibility: 'hidden', // Alternatives will be 'visible' or 'peeking'
   toggleCartVisibility: (visibility) => set((state) => ({ ...state, cartVisibility: visibility })),
   cart: [],
